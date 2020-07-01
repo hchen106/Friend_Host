5d15e52d5657e37a5d3f041f2fdf93990a1dbc31
Matthew <1matthew779@gmail.com>
2020-07-01 01:50:56 -0700

    Udp connection testing

---
+++ testing/udp_client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+import time
+class client:
+
+    def __init__(self):
+        self.ip = "localhost"
+        self.port = 4004
+        self.ADDR = (self.ip, self.port)
+        self.establish_connection()
+
+
+    def tcp_connection(self):
+        print("In TCP connection")
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        time.sleep(2)
+        self.tcp_socket.connect((self.ip  ,self.port+1))
+        self.tcp_socket.send(b'hi')
+
+        while True:
+            
+            data= self.tcp_socket.recv(4096)
+            if(data != b''):
+                self.ad = data.decode("utf-8")
+                print(data)
+                break
+    
+    def establish_connection(self):
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_socket.sendto(b'hi',self.ADDR)
+
+
+        #self.tcp_connection()
+        self.recv_connection()
+        
+
+    def recv_connection(self):
+        
+        #self.receive_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self.receive_socket.bind(("localhost",int(self.ad)))
+
+        while True:
+            data, addr = self.udp_socket.recvfrom(4096)
+            if(data):
+                print(data)
+        
+
+client()
---
+++ testing/udp_server.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+class server:
+
+    def __init__(self):
+        self.ip = "localhost"
+        self.port = 4004
+        self.ADDR = (self.ip, self.port)
+        #self.tcp_connecion()
+        self.receive_connection()
+
+    def tcp_connecion(self):
+        print("In Tcp_connection")
+        self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.tcp_socket.bind(('',self.port+1))
+        self.tcp_socket.listen(5)
+        client = self.tcp_socket.accept()
+        
+        while True:
+            
+            print("accept socket")
+            data= client[0].recv(4096)
+            print(data)
+            mes = bytes(str(self.addr[1]), 'utf-8')
+            client[0].send(mes)
+            break
+
+
+    def receive_connection(self):
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_socket.bind(self.ADDR)
+        self.sender_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        
+        while True:
+            data, self.addr = self.udp_socket.recvfrom(4096)
+            print(self.addr)
+            if(data):
+                #self.tcp_connecion()
+                break
+        
+        self.sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sender_socket.sendto(b'how are you',(self.addr))
+
+server()
